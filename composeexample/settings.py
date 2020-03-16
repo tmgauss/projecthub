6edfa9fafed553fc8862a640df805894412f4740
tmgauss <steveforever7822@gmail.com>
2020-03-16 23:57:54 +0900

    Change basic settings and make homepage's html

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cms.apps.CmsConfig',
 ]
 
 MIDDLEWARE = [
@@ -80,7 +81,7 @@ DATABASES = {
         'USER': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
-        'PASSWORD': 'somepassword'
+        'PASSWORD': 'somepassword',
     }
 }
 
@@ -122,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
